96b563ed1f50a7aef333dc7d8b01fd7f4758f892
Fernando Lara <fmaia8@hotmail.com>
2023-04-03 15:25:55 -0300

    exercicios importantes for/in e leitura de objetos

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-04-objetos/scripts🚀.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-04-objetos/scripts🚀.js
@@ -10,14 +10,44 @@ let info = {
 
 // 1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo o nome dela. Para isso, utilize a sintaxe meuObjeto.chave.
 
+console.log(`boas vindas ${info.personagem}`);
+
 // 2 - Insira no objeto uma nova propriedade com o nome de chave ‘recorrente’ e o valor ‘Sim’ e, em seguida, imprima o objeto no console. Para isso, use a sintaxe meuObjeto['chave'] = valor.
 
-// 3 - Faça um for/in que mostre todas as chaves do objeto. O valor esperado no console é:
+info.recorrente = "sim";
+
+console.log(info);
+
+// 3 - Faça um for/in que mostre todas as chaves do objeto.
+
+for (let key in info) {
+  console.log(key);
+}
 
 // 4 - Faça um novo for/in, mas agora mostre todos os valores das propriedades do objeto.
 
+for (let key in info) {
+  console.log(info[key]);
+}
+
 // 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: “Tio Patinhas”, “Christmas on Bear Mountain, Dell’s Four Color Comics #178”, “O último MacPatinhas”, “Sim”. Então, imprima os valores de cada objeto juntos, de acordo com cada uma das chaves.
 
+let info2 = {
+  personagem: "Tio Patinhas",
+
+  origem: "Christmas on Bear Mountain, Dell’s Four Color Comics #178",
+
+  nota: "O último MacPatinhas",
+  recorrente: "sim",
+};
+
+for (let key in info) {
+  if (key === "recorrente" && info[key] === "sim" && info2[key] === "sim") {
+    console.log("Ambos recorrentes");
+  } else {
+    console.log(info[key] + " & " + info2[key]);
+  }
+}
 // -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // 🚀 Leitura de objetos
@@ -38,7 +68,22 @@ let leitor = {
 };
 
 // 1 - Acesse as chaves nome, sobrenome e titulo – esta encontra-se dentro da chave livrosFavoritos – e faça um console.log no seguinte formato: “O livro favorito de Julia Pessoa se chama ‘O Pior Dia de Todos’”.
+console.log(
+  `O livro favorito de ${leitor.nome} se chama ${leitor.livrosFavoritos[0].titulo}`
+);
 
 // 2 - Adicione um novo livro favorito na chave livrosFavoritos, que é um array. Atribua a essa chave um objeto que contenha as seguintes informações:
 
+leitor.livrosFavoritos.push({
+  titulo: "Harry Potter e o Prisioneiro de Azkaban",
+
+  autor: "JK Rowling",
+
+  editora: "Rocco",
+});
+
+console.log(leitor.livrosFavoritos);
+
 // 3 - Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: “Julia tem {quantidade} livros favoritos”, em que “{quantidade}” corresponde à quantidade de livros favoritos e é um número gerado automaticamente por seu código.
+
+console.log(` Julia tem ${leitor.livrosFavoritos.length} livros favoritos `);
