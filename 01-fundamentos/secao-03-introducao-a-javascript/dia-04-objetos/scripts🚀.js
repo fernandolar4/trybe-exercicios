02646a424138868f06935d1e0e7f342d673a1012
Fernando Lara <fmaia8@hotmail.com>
2023-04-03 17:07:56 -0300

    .

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-04-objetos/scripts🚀.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-04-objetos/scripts🚀.js
@@ -32,7 +32,11 @@ for (let key in info) {
 
 // 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: “Tio Patinhas”, “Christmas on Bear Mountain, Dell’s Four Color Comics #178”, “O último MacPatinhas”, “Sim”. Então, imprima os valores de cada objeto juntos, de acordo com cada uma das chaves.
 
-let info2 = {
+// let info2 = Object.assign({}, info);
+
+// console.table(info2);
+
+info2 = {
   personagem: "Tio Patinhas",
 
   origem: "Christmas on Bear Mountain, Dell’s Four Color Comics #178",
